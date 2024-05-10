25cf6b6d509a3913eb5bf5defed67eb34a5ad569
nakami-777 <namiki.nakano@flierinc.com>
2024-05-10 23:39:16 +0900

    fix

--- terraform/modules/vpc_endpoint/main.tf
+++ terraform/modules/vpc_endpoint/main.tf
@@ -9,41 +9,41 @@ resource "aws_vpc_endpoint" "s3" {
   }
 }
 
-resource "aws_vpc_endpoint" "ecr_api" {
-  vpc_id              = var.vpc_id
-  service_name        = "com.amazonaws.ap-northeast-1.ecr.api"
-  vpc_endpoint_type   = "Interface"
-  subnet_ids          = [var.subnet_id]
-  private_dns_enabled = true
-  security_group_ids  = [var.security_group_id]
+# resource "aws_vpc_endpoint" "ecr_api" {
+#   vpc_id              = var.vpc_id
+#   service_name        = "com.amazonaws.ap-northeast-1.ecr.api"
+#   vpc_endpoint_type   = "Interface"
+#   subnet_ids          = [var.subnet_id]
+#   private_dns_enabled = true
+#   security_group_ids  = [var.security_group_id]
 
-  tags = {
-    Name = "${var.tag_name}_vpc_endpoint_ecr_api"
-  }
-}
+#   tags = {
+#     Name = "${var.tag_name}_vpc_endpoint_ecr_api"
+#   }
+# }
 
-resource "aws_vpc_endpoint" "ecr_dkr" {
-  vpc_id              = var.vpc_id
-  service_name        = "com.amazonaws.ap-northeast-1.ecr.dkr"
-  vpc_endpoint_type   = "Interface"
-  subnet_ids          = [var.subnet_id]
-  private_dns_enabled = true
-  security_group_ids  = [var.security_group_id]
+# resource "aws_vpc_endpoint" "ecr_dkr" {
+#   vpc_id              = var.vpc_id
+#   service_name        = "com.amazonaws.ap-northeast-1.ecr.dkr"
+#   vpc_endpoint_type   = "Interface"
+#   subnet_ids          = [var.subnet_id]
+#   private_dns_enabled = true
+#   security_group_ids  = [var.security_group_id]
 
-  tags = {
-    Name = "${var.tag_name}_vpc_endpoint_ecr_dkr"
-  }
-}
+#   tags = {
+#     Name = "${var.tag_name}_vpc_endpoint_ecr_dkr"
+#   }
+# }
 
-resource "aws_vpc_endpoint" "cloudwatch_logs" {
-  vpc_id              = var.vpc_id
-  service_name        = "com.amazonaws.ap-northeast-1.logs"
-  vpc_endpoint_type   = "Interface"
-  subnet_ids          = [var.subnet_id]
-  private_dns_enabled = true
-  security_group_ids  = [var.security_group_id]
+# resource "aws_vpc_endpoint" "cloudwatch_logs" {
+#   vpc_id              = var.vpc_id
+#   service_name        = "com.amazonaws.ap-northeast-1.logs"
+#   vpc_endpoint_type   = "Interface"
+#   subnet_ids          = [var.subnet_id]
+#   private_dns_enabled = true
+#   security_group_ids  = [var.security_group_id]
 
-  tags = {
-    Name = "${var.tag_name}_vpc_endpoint_cloudwatch_logs"
-  }
-}
+#   tags = {
+#     Name = "${var.tag_name}_vpc_endpoint_cloudwatch_logs"
+#   }
+# }
