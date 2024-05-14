f0caebf7b6029cad508ea0ff0b7902c55cf01504
nakami-777 <namiki.nakano@flierinc.com>
2024-05-15 02:34:07 +0900

    fix

---
+++ front/app/template.tsx
@@ -0,0 +1,9 @@
+export default function Template({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <h1>template</h1>
+      {children}
+      <h1>template</h1>
+    </>
+  );
+}
