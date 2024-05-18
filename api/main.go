607c73189bab97dc9ff90dc6ed4b82f25951ee38
nakami-777 <namiki.nakano@flierinc.com>
2024-05-18 18:29:03 +0900

    fix

--- api/main.go
+++ api/main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -102,6 +103,33 @@ func putAlbumByID(c *gin.Context) {
 
 func main() {
 	router := gin.Default()
+
+	router.Use(
+		cors.New(
+			cors.Config{
+				AllowOrigins: []string{
+					"http://localhost:3000",
+					"http://helloworld-dev-alb-front-1863607102.ap-northeast-1.elb.amazonaws.com",
+				},
+				AllowMethods: []string{
+					"GET",
+					"PUT",
+					"POST",
+					"DELETE",
+					"OPTIONS",
+				},
+				AllowHeaders: []string{
+					"Access-Control-Allow-Credentials",
+					"Access-Control-Allow-Headers",
+					"Content-Type",
+					"Content-Length",
+					"Accept-Encoding",
+					"Authorization",
+				},
+				AllowCredentials: true,
+			},
+		),
+	)
 
 	v1 := router.Group("/v1")
 	{
--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,3 +1,43 @@
-export default function Home() {
-  return <div>posts</div>;
+"use client";
+
+import axios from "axios";
+import { headers } from "next/headers";
+import { useEffect, useState } from "react";
+
+interface Album {
+  id: number;
+  title: string;
+  artist: string;
+  price: number;
 }
+
+function Home() {
+  const [albums, setAlbums] = useState<Album[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // const res = await axios.get(
+      //   "http://helloworld-dev-alb-api-2082722876.ap-northeast-1.elb.amazonaws.com/v1/albums"
+      // );
+      const res = await axios.get("http://localhost:8080/v1/albums");
+      setAlbums(res.data);
+    };
+    fetchData();
+  });
+
+  return (
+    <div>
+      {albums.map((album) => {
+        return (
+          <div key={album.id}>
+            <h1>{album.title}</h1>
+            <p>{album.artist}</p>
+            <p>{album.price}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Home;
